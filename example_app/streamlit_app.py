e3387d1a1d30b0b7cd9ca523a629a42f3e3e66ac
Daniel Martinez <57229989+danielq1985@users.noreply.github.com>
2024-07-10 09:59:12 -0600

    Update streamlit_app.py

--- example_app/streamlit_app.py
+++ example_app/streamlit_app.py
@@ -1,22 +1,16 @@
 import streamlit as st
 from streamlit_back_camera_input import back_camera_input
-
-image = back_camera_input()
-if image:
-    st.image(image)
-"""
 import cv2
 import numpy as np
-import streamlit as st
 from camera_input_live import camera_input_live
 
 
 
 
-"# Streamlit camera input live Demo"
-"## Try holding a qr code in front of your webcam"
+"# Test app"
 
-image = camera_input_live()
+##image = camera_input_live()
+image = back_camera_input()
 
 if image is not None:
     st.image(image)
@@ -33,4 +27,9 @@ if image is not None:
         with st.expander("Show details"):
             st.write("BBox:", bbox)
             st.write("Straight QR code:", straight_qrcode)
+
+"""
+image = back_camera_input()
+if image:
+    st.image(image)
 """
