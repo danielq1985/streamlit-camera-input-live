3b0bdc5d1fd55320061409d732b83d8c0bf8698c
Daniel Martinez <57229989+danielq1985@users.noreply.github.com>
2024-07-11 10:06:09 -0600

    Update streamlit_app.py

--- example_app/streamlit_app.py
+++ example_app/streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 import cv2
-import numpy as no
+import numpy as np
 
 st.set_page_config(layout="wide")
 st.title("Test App")
@@ -15,27 +15,15 @@ with col_left:
 with col_right:
     if x is not None:
         st.image(x.getvalue(), caption="Your photo", use_column_width=True)
+        bytes_data = x.getvalue()
+        cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
 
+        detector = cv2.QRCodeDetector()
+        data, bbox, straight_qrcode = detector.detectAndDecode(cv2_img)
 
-##image = camera_input_live()
-#$image = back_camera_input()
-image = x
-
-
-if image is not None:
-    st.image(image)
-    bytes_data = image.getvalue()
-    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
-
-    detector = cv2.QRCodeDetector()
-
-    data, bbox, straight_qrcode = detector.detectAndDecode(cv2_img)
-
-    if data:
-        st.write("# Found QR code")
-        st.write(data)
-        ##with st.expander("Show details"):
-          ##  st.write("BBox:", bbox)
-          ##  st.write("Straight QR code:", straight_qrcode)
-
-          
+        if data:
+            st.write("# Found QR code")
+            st.write(data)
+            ##with st.expander("Show details"):
+            ##  st.write("BBox:", bbox)
+            ##  st.write("Straight QR code:", straight_qrcode)
