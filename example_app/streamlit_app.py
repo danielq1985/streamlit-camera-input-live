43477c4e35e212cde7744447e2cb5348b0313425
Daniel Martinez <57229989+danielq1985@users.noreply.github.com>
2024-07-11 08:19:56 -0600

    Update streamlit_app.py

--- example_app/streamlit_app.py
+++ example_app/streamlit_app.py
@@ -5,7 +5,21 @@ import numpy as np
 from camera_input_live import camera_input_live
 
 
+import requests
 
+st.set_page_config(layout="wide")
+st.title("Camera input demo app. Take photo and see it toonified!")
+
+col_left, _, col_right = st.columns([5, 1, 5])
+
+
+with col_left:
+    x = st.camera_input(label="AAA", key="camera_input_file")
+
+with col_right:
+    if x is not None:
+        st.image(x.getvalue(), caption="Your photo", use_column_width=True)
+"""
 
 "# Test app"
 "### tap image when centered and in focus"
